74b7ab2f395bbbc6b2773571df3640d373bea3ec
Korada BhavanaSri <bhavanakorada312@gmail.com>
2026-03-23 22:19:46 +0530

    fixed port

--- app.py
+++ app.py
@@ -175,6 +175,7 @@ def schedule():
         )
 # ---------------- RUN ----------------
 
-
+git init
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port=int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
